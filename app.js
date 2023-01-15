b21ce240871012ff62b68844adaf68ac4c5d9476
Juliandresmm <home@iMac-de-Home.local>
2023-01-14 23:31:24 -0500

    Primera versión de mercado liebre

---
+++ app.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+
+app.use(express.static('public'));
+
+const port = 3000;
+
+
+app.listen(port,()=> console.log("Servidor corriendo en el puerto "+ port));
+
+// app.get('/', function(req, res) {
+//     res.send('Hola Mundo')
+// });
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '/views/home.html'));
+});
