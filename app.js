71ee02cc255efb26bac70869ebee7319a075bea4
Juliandresmm <home@iMac-de-Home.local>
2023-01-24 16:20:32 -0500

    Primera version completa de Mercadoliebre

--- app.js
+++ app.js
@@ -21,3 +21,11 @@ app.get('/', (req, res) => {
 app.get('/productDetail', (req, res) => {
     res.sendFile(path.join(__dirname, '/views/productDetail.html'));
 });
+
+app.get('/login', (req, res) => {
+    res.sendFile(path.join(__dirname, '/views/login.html'));
+});
+
+app.get('/register', (req, res) => {
+    res.sendFile(path.join(__dirname, '/views/register.html'));
+});
